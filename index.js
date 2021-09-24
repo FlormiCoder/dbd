12e47a62ca32725682e7bdcb90341e322f61cdb0
Flormi <75210082+FlormiCoder@users.noreply.github.com>
2021-09-24 07:28:11 +0300

    Revert "Update index.js"

--- index.js
+++ index.js
@@ -12,21 +12,6 @@ name: "пинг", //Trigger(Триггер команды)
 code: `пинг бота $ping` //Code(Код команды)
 })
 
-module.exports = ({
-  name: "система",
-  aliases: ["sysinfo", "system"],
-  usage: "sys",
-  description: "Выдает статы систимы",
-  code: `
-$title[Информация о системе]
-  $addField[Система;**CPU:** $djsEval[require("os").cpus()[0].model;yes]
-**ОС:** $djsEval[require("os").platform;yes] x64]
-  $addField[Память;**Доступно:** $sub[$maxRam;$ram]mb
-**Всего:** $maxRammb
-**Потребление:** $rammb]
-$color[00ff00]`
-})
-
 //status(статус бота)
 
 bot.status({
