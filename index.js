d6f82aa9467b0fb8c4c2042ad341c5f0ddbfc292
Flormi <75210082+FlormiCoder@users.noreply.github.com>
2021-09-24 11:56:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ bot.command({
 })
 
 bot.command({
+  name: "префикс",
+  aliases: ["sprefix", "sp", "prefix"],
+  code: `
+  $sendMessage[
+    {title:Префикс успешно изменён!}
+    {description:**Новый префикс\:** \`$message[1]\`}
+  ;no]
+  $setServerVar[prefix;$message[1]]
+  $onlyIf[$charCount[$message[1]]<=3;:x:\`Префикс не может быть длиннее 3 символов!\`]
+  $onlyIf[$message[1]!=;:x:\`Префикс не может быть пустым!\`]
+  $onlyPerms[manageserver;:x:\`У вас недостаточно прав для смены префикса!\`]
+  `
+})
+
+bot.command({
 name: "пинг", //Trigger(Триггер команды)
 code: `пинг бота $ping` //Code(Код команды)
 })
