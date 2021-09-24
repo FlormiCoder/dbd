67b83a2f9f6843406dec9b93c543a5103779df51
Flormi <75210082+FlormiCoder@users.noreply.github.com>
2021-09-24 19:36:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -129,6 +129,7 @@ bot.command({
   $thumbnail[$userAvatar[$findUser[$message[1]]]]
   $title[Информация о пользователе - $username[$findUser[$message[1]]]]
   $description[**Имя** - $username[$findUser[$message[1]]]
+**Сообщений** - $GetGlobalUserVar[message;$mentioned[1;yes]
 **Статус** - $replaceText[$replaceText[$replaceText[$replaceText[$status[$findUser[$message[1]]];online;В сети;-1];offline;Не в сети;-1];idle;Не активен;-1];dnd;Не беспокоить;-1]
 **Пользовательский статус** - $replaceText[$replaceText[$checkCondition[$getCustomStatus[$findUser[$message];emote]$getCustomStatus[$findUser[$message];state]==nonenone];true;Отсутствует];false;$replaceText[$replaceText[$checkCondition[$getCustomStatus[$findUser[$message];emote]$getCustomStatus[$findUser[$message];state]==$getCustomStatus[$findUser[$message];emote]none];true;$getCustomStatus[$findUser[$message];emote]];false;$replaceText[$replaceText[$checkCondition[$getCustomStatus[$findUser[$message];emote]$getCustomStatus[$findUser[$message];state]==none$getCustomStatus[$findUser[$message];state]];true;$getCustomStatus[$findUser[$message];state]];false;$getCustomStatus[$findUser[$message];emote] $getCustomStatus[$findUser[$message];state]]
 **Платформа** - $replaceText[$replaceText[$replaceText[$replaceText[$platform[$findUser[$message[1]]];none;❓ Отсутствует;-1];web;📄 Браузер;-1];mobile;📱 Телефон;-1];desktop;🖥️ Компьютер;-1]
