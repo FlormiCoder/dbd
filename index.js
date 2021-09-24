f643d560b890593ae5698ad5812dec7a701362f6
Flormi <75210082+FlormiCoder@users.noreply.github.com>
2021-09-24 10:28:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,31 @@ $title[Информация о системе]
 $color[00ff00]`
 })
 
+bot.command({
+name: "сервер",
+aliases: ["server-info","si"],
+code: `
+$reply[$messageID;
+{color:313131}
+{title:Информация о сервере}
+{field:Владелец: <@$ownerID> | $username[$ownerID]$discriminator[$ownerID]}
+{field:ID сервера: \`$guildID\`}
+{field:Регион: \`$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$serverRegion;brazil;Бразилия];russia;Россия];europe;Европы];hong kong;Гонконг];india;Индия];Japane;Япония];Singapore;Сингапур];south africa;Южная Африка];sydney;Сидней];us central;Центральная часть Сша];us west;Западная часть США];us east;Восточная часть США];us south; Южная часть США]\`}
+{field:Уровень проверки: \`$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$serverVerificationLevel;None;Выключено];Low;Низкий];Medium;Средний];High;Высокий];Very High;Очень высокий]\`}
+{field:Фильтр содержимого: \`$replaceText[$replaceText[$replaceText[$serverContentFilter;All Members;Фильтрация всех пользователей];Members Without Roles;Фильтрация пользователей без роли];Disabled;Фильтрация отключена]\`}
+{field:Канал системных сообщений: $replaceText[<#$systemChannelID>;<#>;Такого канала нет]}
+{field:Уровень буста: \`$serverBoostLevel\`}
+{field:Всего бустов: \`$serverBoostCount\`}
+{field:Забаненых: \`$replaceText[$banCount;missing permissions;У меня нет прав на просмотр забаненых пользователей]\`}
+{field:Человек: \`$membersCount\`}
+{field:Ботов: \`$botCount\`}
+{field:Каналов: \`$channelCount\`}
+{field:Ролей: \`$roleCount\`}
+{field:Эмодзей: \`$emojiCount\`}
+{field:Инвайт: $getServerInvite}]
+`
+})
+
 //status(статус бота)
 
 bot.status({
