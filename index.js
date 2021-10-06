10e70c15e01f151fbee6262e724a7e43c7184993
Flormi <75210082+FlormiCoder@users.noreply.github.com>
2021-10-07 00:24:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,21 @@ bot.loopCommand({
 })
 
 bot.command({
-  name: "плэй",
-  code: `$playSong[$message;2m;yes;yes;Не удалось найти музыку!]`
+ name: "плей",
+ aliases: ['p'],
+ code: `
+$color[RANDOM]
+$thumbnail[$jsonRequest[http://api.somecool.repl.co/yt-search?search=$message;thumbnail]]
+$title[**Песня добавлена в очередь**]
+$description[**Succesfully added** [$songInfo[title]\\]($songInfo[url]) to the **Queue**]
+$addField[:stopwatch:| Duration:;**__$jsonRequest[http://api.somecool.repl.co/yt-search?search=$message;duration]__**]
+$addField[:dvd: | Views:;**__$jsonRequest[http://api.somecool.repl.co/yt-search?search=$message;views]__**]
+$addField[:coffee: | Author:;**__$jsonRequest[http://api.somecool.repl.co/yt-search?search=$message;uploader_name]__**]
+$addField[:clock10: Uploaded:;**__$jsonRequest[http://api.somecool.repl.co/yt-search?search=$message;uploaded]__**]
+$playSong[$message;1m;{title:Error}{description:**⛔ There was an error while making the request**}{color:RED}]
+$onlyIf[$message!=;{title:Error}{description:**⛔ I need Song name to find a** \`song\`...}]
+$onlyIf[$voiceID!=;**⛔ You are Not in a Voice channel! Join a voice channel**]
+$cooldown[5s;Подожди **%time%**, чтобы снова использовать эту команду]`
 })
 
 bot.command({
